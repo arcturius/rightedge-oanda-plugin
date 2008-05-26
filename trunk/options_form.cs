0c94c465fd2c681aed0db84d0bf8a8f0fe272ffb
mark_loftis@earthlink.net <mark_loftis@earthlink.net@6a34c06c-1d4d-0410-9736-67829504de5b>
2008-05-26 04:10:34 +0000

    Initial import

---
+++ trunk/options_form.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OandA_RightEdge_Plugin
+{
+    public partial class OandAPluginOptionsForm : Form
+    {
+        private OAPluginOptions _opts;
+        public OAPluginOptions Opts { get { return (_opts); } set { _opts = value; oandAPluginOptionsControl1.Opts = _opts; } }
+
+        public OandAPluginOptionsForm(OAPluginOptions opts)
+        {
+            InitializeComponent();
+            _opts = opts;
+            oandAPluginOptionsControl1.Opts = _opts;
+        }
+
+        public OandAPluginOptionsForm()
+        {
+            InitializeComponent();
+            _opts = new OAPluginOptions();
+            oandAPluginOptionsControl1.Opts = _opts;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void cancelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
